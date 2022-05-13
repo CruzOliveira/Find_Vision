4cffe044b31616958478d74e8fdce4125c78bffe
CruzOliveira <80063865+CruzOliveira@users.noreply.github.com>
2022-05-13 15:06:43 -0300

    quase finalizado o projeto

--- FindVision/src/component/Cadastro.js
+++ FindVision/src/component/Cadastro.js
@@ -1,86 +1,94 @@
 import { StatusBar } from "expo-status-bar";
 import React, {useState} from "react";
-import { TextInput, TouchableOpacity, View, Image, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { ScrollView, TextInput, TouchableOpacity, View, Image, Text, StyleSheet, SafeAreaView } from 'react-native'
 import { Checkbox } from 'react-native-paper'
 import {Ionicons} from '@expo/vector-icons';
-import { ScrollView } from "react-native-web";
 
 export default function Cadastrar(){
 
-    const [checked, setChecked] = React.useState(false);
+    const [checked, setChecked] = useState(false);
     const [input, setInput] = useState('');
     const [hidePass, setHidePass] = useState(true);
     return(
     <SafeAreaView style={styles.conteinerPai}>
-        <StatusBar backgroundColor="#FF9400" />
         <ScrollView>
-            <Image style={styles.Logo} source={require('../img/LogoCadastro.png')}
-            />
-            <View style={styles.conteinerCadastro}>
-                <Text style={styles.title}>Nome</Text>
-                <View style={styles.areaInputNome}>
-                <TextInput style={styles.Input}/>
-                </View>
-                <Text style={styles.title}>Email</Text>
-                <View style={styles.areaInputEmail}>
-                <TextInput style={styles.Input}/>
-                </View>
-                <Text style={styles.title}>Senha</Text>
-                <View style={styles.areaInputSenha}>
-                <TextInput 
-                value={input}
-                onChangeText={(text) => setInput(text)}
-                secureTextEntry={hidePass}
-                style={styles.InputSenha}/>
-                <TouchableOpacity
-                onPress={() => setHidePass(!hidePass)}
-                style={styles.icon}>
-                    <Ionicons name="eye" color='#BBB5AA' size={25}/>
-                </TouchableOpacity>
-                </View>
-                <View style={styles.areaText}>
-                    <Text style={styles.textCPF}>CPF</Text>
-                    <Text   style={styles.textNacimento}>D/NACIMENTO</Text>
-                </View>
-                <View style={styles.areaInputCPFNacimento}>
-                    
-                    <View style={styles.areaInputCPF}>
-                        <TextInput style={styles.InputDuplo}/>
-                    </View>
-                    
-                    <View style={styles.areaInputNacimento}>
-                        <TextInput style={styles.InputDuplo}/>
-                    </View>
-                </View>
-
-                <Text style={styles.title}>Numero</Text>
-                <View style={styles.areaInputNumero}>
-                <TextInput 
-                placeholder="(00)0000-0000"
-                style={styles.Input}/>
-                </View>
-                <Text style={styles.title}>Cidade</Text>
-                <View style={styles.areaInputCidade}>
-                <TextInput style={styles.Input}/>
-                </View>
-
-                <View>
-                    <Checkbox
-                    status={checked ? 'checked' : 'unchecked'}
-                    onPress={() => {
-                    setChecked2(!checked);
-                    }}
-                    /><Text>
-                        Concordo com os Termos e Condições
-                    </Text>
-                </View>
-                <View>
-                    <TouchableOpacity>
-                        <Text>CADASTRAR</Text>
-                    </TouchableOpacity>
+        <StatusBar backgroundColor="#FF9400" />
+        <Image style={styles.Logo} source={require('../img/LogoCadastro.png')}
+        />
+        <View style={styles.conteinerCadastro}>
+            <Text style={styles.title}>Nome</Text>
+            <View style={styles.areaInputNome}>
+            <TextInput style={styles.Input}/>
+            </View>
+            <Text style={styles.title}>Email</Text>
+            <View style={styles.areaInputEmail}>
+            <TextInput style={styles.Input}/>
+            </View>
+            <Text style={styles.title}>Senha</Text>
+            <View style={styles.areaInputSenha}>
+            <TextInput 
+            value={input}
+            onChangeText={(text) => setInput(text)}
+            secureTextEntry={hidePass}
+            style={styles.InputSenha}/>
+            <TouchableOpacity
+            onPress={() => setHidePass(!hidePass)}
+            style={styles.icon}>
+                <Ionicons name="eye" color='#BBB5AA' size={25}/>
+            </TouchableOpacity>
+            </View>
+            <View style={styles.areaText}>
+                <Text style={styles.textCPF}>CPF</Text>
+                <Text style={styles.textNacimento}>D/NACIMENTO</Text>
+            </View>
+            <View style={styles.areaInputCPFNacimento}>
+                
+                <View style={styles.areaInputCPF}>
+                    <TextInput 
+                    keyboardType='numeric'
+                    placeholder="000.000.000-00"
+                    style={styles.InputDuplo}/>
                 </View>
                 
+                <View style={styles.areaInputNacimento}>
+                    <TextInput 
+                    keyboardType='numeric'
+                    placeholder="00/00/0000"
+                    style={styles.InputDuplo}/>
+                </View>
             </View>
+
+            <Text style={styles.title}>Numero</Text>
+            <View style={styles.areaInputNumero}>
+            <TextInput 
+            placeholder="(00)0000-0000"
+            keyboardType='numeric'
+            style={styles.Input}/>
+            </View>
+            <Text style={styles.title}>Cidade</Text>
+            <View style={styles.areaInputCidade}>
+            <TextInput style={styles.Input}/>
+            </View>
+
+            <View style={styles.areaCheckbox}>
+                <Checkbox
+                uncheckedColor="#D2CEC5"
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={() => {
+                setChecked(!checked);
+                }}
+                />
+                <Text style={styles.textCheckbox}>
+                    Concordo com os Termos e Condições
+                </Text>
+            </View>
+            <View style={styles.areaButton}>
+                <TouchableOpacity>
+                    <Text style={styles.textButton}>CADASTRAR</Text>
+                </TouchableOpacity>
+            </View>
+            
+        </View>
         </ScrollView>
     </SafeAreaView>
 )};
@@ -95,9 +103,37 @@ const styles = StyleSheet.create({
         backgroundColor:'#5F5B5B',
         marginHorizontal: 20,
         width: 370,
-        height: 500,
+        height: 670,
         borderRadius: 4,
         elevation: 10,
+        marginBottom:30,
+    },
+    InputDuplo:{
+        width:'98%',
+        height: 41,
+        color:'black',
+        fontSize:16,
+        marginLeft:5,
+    },
+    areaButton:{
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#FF9400',
+        height: 50,
+        width: 177,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor:'#B18739',
+        elevation: 10,
+        marginHorizontal:'25%',
+        marginTop: 30,
+
+
+    },
+    textButton:{
+        fontSize:16,
+        color:'#4A4747',
+        fontWeight: '700',
     },
 
     Logo:{
@@ -106,6 +142,13 @@ const styles = StyleSheet.create({
         marginHorizontal:20,
         marginTop:55,
         marginBottom:20,
+    },
+
+    areaCheckbox:{
+        flexDirection:'row',
+        marginTop:20,
+        marginLeft:15,
+        elevation: 20,
     },
 
     areaInputNome:{
@@ -234,6 +277,13 @@ const styles = StyleSheet.create({
         color:'#BA872A',
         fontWeight: '700',
     },
+    textCheckbox:{
+        marginTop:7,
+        fontSize:16,
+        color:'#BA872A',
+        fontWeight: '700',
+        elevation: 20,
+    },
     textCPF:{
         marginTop:20,
         fontSize:16,
@@ -247,9 +297,5 @@ const styles = StyleSheet.create({
         color:'#BA872A',
         fontWeight: '700',
     },
-    
-
-
-
 
 });
