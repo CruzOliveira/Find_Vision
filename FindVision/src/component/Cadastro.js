49531be0c427045a0942fc15900f8604b5754f32
Matheus Cruz <cruzoliveira2611@gmail.com>
2022-09-09 22:08:48 -0300

    commit

--- FindVision/src/component/Cadastro.js
+++ FindVision/src/component/Cadastro.js
@@ -1,302 +1,312 @@
-import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
-import { ScrollView, TextInput, TouchableOpacity, View, Image, Text, StyleSheet, SafeAreaView } from 'react-native'
-import { Checkbox } from 'react-native-paper'
-import {Ionicons} from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
+import { Checkbox } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+const Cadastro = ({ navigation }) => {
+  const [checked, setChecked] = useState(false);
+  const [input, setInput] = useState('');
+  const [hidePass, setHidePass] = useState(true);
 
-const Cadastro = ({navigation}) =>{
-
-    const [checked, setChecked] = useState(false);
-    const [input, setInput] = useState('');
-    const [hidePass, setHidePass] = useState(true);
-    return(
+  return (
     <SafeAreaView style={styles.conteinerPai}>
-        <ScrollView>
+      <ScrollView>
         <StatusBar backgroundColor="#FF9400" />
-        <Image style={styles.Logo} source={require('../img/LogoCadastro.png')}
+        <Image
+          style={styles.Logo}
+          source={require('../img/LogoCadastro.png')}
         />
         <View style={styles.conteinerCadastro}>
-            <Text style={styles.title}>Nome</Text>
-            <View style={styles.areaInputNome}>
-            <TextInput style={styles.Input}/>
-            </View>
-            <Text style={styles.title}>Email</Text>
-            <View style={styles.areaInputEmail}>
-            <TextInput style={styles.Input}/>
-            </View>
-            <Text style={styles.title}>Senha</Text>
-            <View style={styles.areaInputSenha}>
-            <TextInput 
-            value={input}
-            onChangeText={(text) => setInput(text)}
-            secureTextEntry={hidePass}
-            style={styles.InputSenha}/>
+          <Text style={styles.title}>Nome</Text>
+          <View style={styles.areaInputNome}>
+            <TextInput style={styles.Input} />
+          </View>
+          <Text style={styles.title}>Email</Text>
+          <View style={styles.areaInputEmail}>
+            <TextInput style={styles.Input} />
+          </View>
+          <Text style={styles.title}>Senha</Text>
+          <View style={styles.areaInputSenha}>
+            <TextInput
+              value={input}
+              onChangeText={(text) => setInput(text)}
+              secureTextEntry={hidePass}
+              style={styles.InputSenha}
+            />
             <TouchableOpacity
-            onPress={() => setHidePass(!hidePass)}
-            style={styles.icon}>
-                <Ionicons name="eye" color='#BBB5AA' size={25}/>
+              onPress={() => setHidePass(!hidePass)}
+              style={styles.icon}
+            >
+              <Ionicons name="eye" color="#BBB5AA" size={25} />
             </TouchableOpacity>
-            </View>
-            <View style={styles.areaText}>
-                <Text style={styles.textCPF}>CPF</Text>
-                <Text style={styles.textNacimento}>D/NACIMENTO</Text>
-            </View>
-            <View style={styles.areaInputCPFNacimento}>
-                
-                <View style={styles.areaInputCPF}>
-                    <TextInput 
-                    keyboardType='numeric'
-                    placeholder="000.000.000-00"
-                    style={styles.InputDuplo}/>
-                </View>
-                
-                <View style={styles.areaInputNacimento}>
-                    <TextInput 
-                    keyboardType='numeric'
-                    placeholder="00/00/0000"
-                    style={styles.InputDuplo}/>
-                </View>
+          </View>
+          <View style={styles.areaText}>
+            <Text style={styles.textCPF}>CPF</Text>
+            <Text style={styles.textNacimento}>D/NACIMENTO</Text>
+          </View>
+          <View style={styles.areaInputCPFNacimento}>
+            <View style={styles.areaInputCPF}>
+              <TextInput
+                keyboardType="numeric"
+                placeholder="000.000.000-00"
+                style={styles.InputDuplo}
+              />
             </View>
 
-            <Text style={styles.title}>Numero</Text>
-            <View style={styles.areaInputNumero}>
-            <TextInput 
-            placeholder="(00)0000-0000"
-            keyboardType='numeric'
-            style={styles.Input}/>
+            <View style={styles.areaInputNacimento}>
+              <TextInput
+                keyboardType="numeric"
+                placeholder="00/00/0000"
+                style={styles.InputDuplo}
+              />
             </View>
-            <Text style={styles.title}>Cidade</Text>
-            <View style={styles.areaInputCidade}>
-            <TextInput style={styles.Input}/>
-            </View>
+          </View>
 
-            <View style={styles.areaCheckbox}>
-                <Checkbox
-                uncheckedColor="#D2CEC5"
-                status={checked ? 'checked' : 'unchecked'}
-                onPress={() => {
+          <Text style={styles.title}>Numero</Text>
+          <View style={styles.areaInputNumero}>
+            <TextInput
+              placeholder="(00)0000-0000"
+              keyboardType="numeric"
+              style={styles.Input}
+            />
+          </View>
+          <Text style={styles.title}>Cidade</Text>
+          <View style={styles.areaInputCidade}>
+            <TextInput style={styles.Input} />
+          </View>
+
+          <View style={styles.areaCheckbox}>
+            <Checkbox
+              uncheckedColor="#D2CEC5"
+              status={checked ? 'checked' : 'unchecked'}
+              onPress={() => {
                 setChecked(!checked);
-                }}
-                />
-                <Text style={styles.textCheckbox}>
-                    Concordo com os Termos e Condições
-                </Text>
-            </View>
-            <View style={styles.areaButton}>
-                <TouchableOpacity onPress={ () => navigation.replace('Login')}>
-                    <Text style={styles.textButton}>CADASTRAR</Text>
-                </TouchableOpacity>
-            </View>
-            
+              }}
+            />
+            <Text style={styles.textCheckbox}>
+              Concordo com os Termos e Condições
+            </Text>
+          </View>
+          <View style={styles.areaButton}>
+            <TouchableOpacity onPress={() => navigation.replace('Login')}>
+              <Text style={styles.textButton}>CADASTRAR</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        </ScrollView>
+      </ScrollView>
     </SafeAreaView>
-)};
+  );
+};
 export default Cadastro;
 const styles = StyleSheet.create({
-    conteinerPai:{
-        flex: 1,
-        backgroundColor:'#464237',
-        fontFamily:'Roboto',
-    },
+  conteinerPai: {
+    flex: 1,
+    backgroundColor: '#464237',
+    fontFamily: 'Roboto',
+  },
 
-    conteinerCadastro:{
-        backgroundColor:'#5F5B5B',
-        marginHorizontal: 20,
-        width: 370,
-        height: 670,
-        borderRadius: 4,
-        elevation: 10,
-        marginBottom:30,
-    },
-    InputDuplo:{
-        width:'98%',
-        height: 41,
-        color:'black',
-        fontSize:16,
-        marginLeft:5,
-    },
-    areaButton:{
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#FF9400',
-        height: 50,
-        width: 177,
-        borderRadius: 4,
-        borderWidth: 2,
-        borderColor:'#B18739',
-        elevation: 10,
-        marginHorizontal:'25%',
-        marginTop: 30,
+  conteinerCadastro: {
+    backgroundColor: '#5F5B5B',
+    marginHorizontal: 10,
+    width: 370,
+    height: 670,
+    borderRadius: 4,
+    elevation: 10,
+    marginBottom: 30,
+  },
+  InputDuplo: {
+    width: '100%',
+    height: 41,
+    color: 'black',
+    fontSize: 16,
+  },
+  areaButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF9400',
+    height: 50,
+    width: 177,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#B18739',
+    elevation: 10,
+    marginHorizontal: '25%',
+    marginTop: 30,
+  },
+  textButton: {
+    fontSize: 16,
+    color: '#4A4747',
+    fontWeight: '700',
+  },
 
+  Logo: {
+    width: 195,
+    height: 42,
+    marginHorizontal: 20,
+    marginTop: 55,
+    marginBottom: 20,
+  },
 
-    },
-    textButton:{
-        fontSize:16,
-        color:'#4A4747',
-        fontWeight: '700',
-    },
+  areaCheckbox: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginLeft: 15,
+    elevation: 20,
+  },
 
-    Logo:{
-        width:195,
-        height:42,
-        marginHorizontal:20,
-        marginTop:55,
-        marginBottom:20,
-    },
+  areaInputNome: {
+    width: '89%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    marginHorizontal: 20,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
 
-    areaCheckbox:{
-        flexDirection:'row',
-        marginTop:20,
-        marginLeft:15,
-        elevation: 20,
-    },
+  Input: {
+    width: '100%',
+    height: 41,
+    color: 'black',
+    fontSize: 16,
+  },
 
-    areaInputNome:{
-        width:'89%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        marginHorizontal:20,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-    },
+  InputSenha: {
+    width: '100%',
+    height: 41,
+    color: 'black',
+    fontSize: 16,
+  },
 
-    Input:{
-        width:'96%',
-        height: 41,
-        color:'black',
-        fontSize:16,
-    },
+  areaInputEmail: {
+    width: '89%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    marginHorizontal: 20,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
 
-    InputSenha:{
-        width:'85%',
-        height: 41,
-        color:'black',
-        fontSize:16,
-        marginLeft:5,
-    },
+  areaInputSenha: {
+    flexDirection: 'row',
+    width: '89%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    marginHorizontal: 20,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
 
-    areaInputEmail:{
-        width:'89%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        marginHorizontal:20,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-    },
+  areaInputCidade: {
+    width: '89%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    marginHorizontal: 20,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
 
-    areaInputSenha:{
-        flexDirection:'row',
-        width:'89%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        marginHorizontal:20,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-    },
-    
-    areaInputCidade:{
-        width:'89%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        marginHorizontal:20,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-    },
+  areaInputNumero: {
+    width: '89%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    marginHorizontal: 20,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
 
-    areaInputNumero:{
-        width:'89%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        marginHorizontal:20,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-    },
+  areaInputCPFNacimento: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
 
-    areaInputCPFNacimento:{
-        flexDirection:'row',
-        marginHorizontal:20,
-    },
+  areaText: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
 
-    areaText:{
-        flexDirection:'row',
-        marginHorizontal:20,
+  areaInputCPF: {
+    backgroundColor: '#F9F8F6',
+    width: '46%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
 
-    },
+  areaInputNacimento: {
+    backgroundColor: '#F9F8F6',
+    marginLeft: 25,
+    width: '46%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
 
-    areaInputCPF:{
-        backgroundColor:'#F9F8F6',
-        width:'46%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-    },
+  icon: {
+    width: '10%',
+    padding: 30,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    areaInputNacimento:{
-        backgroundColor:'#F9F8F6',
-        marginLeft: 25,
-        width:'46%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-    },
-
-    icon:{
-        width:'15%',
-        height:40,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-
-    title:{
-        marginHorizontal:20,
-        marginTop:20,
-        fontSize: 16,
-        color:'#BA872A',
-        fontWeight: '700',
-    },
-    textCheckbox:{
-        marginTop:7,
-        fontSize:16,
-        color:'#BA872A',
-        fontWeight: '700',
-        elevation: 20,
-    },
-    textCPF:{
-        marginTop:20,
-        fontSize:16,
-        color:'#BA872A',
-        fontWeight: '700',
-    },
-    textNacimento:{
-        marginTop: 20,
-        marginLeft:152,
-        fontSize:16,
-        color:'#BA872A',
-        fontWeight: '700',
-    },
-
+  title: {
+    marginHorizontal: 20,
+    marginTop: 20,
+    fontSize: 16,
+    color: '#BA872A',
+    fontWeight: '700',
+  },
+  textCheckbox: {
+    marginTop: 7,
+    fontSize: 16,
+    color: '#BA872A',
+    fontWeight: '700',
+    elevation: 20,
+  },
+  textCPF: {
+    marginTop: 20,
+    fontSize: 16,
+    color: '#BA872A',
+    fontWeight: '700',
+  },
+  textNacimento: {
+    marginTop: 20,
+    marginLeft: 152,
+    fontSize: 16,
+    color: '#BA872A',
+    fontWeight: '700',
+  },
 });
--- FindVision/src/component/Login.js
+++ FindVision/src/component/Login.js
@@ -1,171 +1,224 @@
-import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
-import { SafeAreaView, Image, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Login = ({navigation}) =>{
-    const [input, setInput] = useState('');
-    const [hidePass, setHidePass] = useState(true);
-    return(
+const Login = ({ navigation }) => {
+  const [hidePass, setHidePass] = useState(true);
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
 
-        <SafeAreaView style={styles.conteinerPai}>
-            <StatusBar backgroundColor="#FF9400" />
-            <View style={styles.conteinerImg}>
-                <Image source={require('../img/Logo.png')}
-                style={styles.logo}/>
-            </View>
-            <Text style={styles.textEmail}>Email</Text>
-            <View style={styles.inputAreaEmail}>
-                <TextInput style={styles.inputEmail}/>
-            </View>
-            <Text style={styles.textSenha}>Senha</Text>
-            <View style={styles.inputArea}>
-                <TextInput style={styles.input}
-                value={input}
-                onChangeText={(texto) => setInput(texto)}
-                secureTextEntry={hidePass}
-                />
-                <TouchableOpacity style={styles.icon} onPress={() => setHidePass(!hidePass)}>
-                    <Ionicons name="eye" color='#BBB5AA' size={25}/>
-                </TouchableOpacity>
+  const onInit = async () => {
+    const ret = await AsyncStorage.getItem('auth');
+    if (ret != null) {
+      navigation.replace('Lista');
+    }
+  };
+  const login = async () => {
+    try {
+      console.log('Login 1', inputEmail + inputPassword);
+      if (inputEmail != '') {
+        console.log('Login2');
+        const response = await fetch('', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            Email: inputEmail,
+            password: inputPassword,
+          }),
+        });
+        console.log('Login 2.1', response);
+        if (response.status == 200) {
+          const obj = await response.json();
+          console.log('Login 3', obj);
+          await AsyncStorage.setItem('auth', JSON.stringify(obj));
+          navigation.replace('Lista');
+        } else {
+          console.log('Login 4');
+          alert('Senha invalida');
+        }
+      }
+    } catch (e) {
+      console.log('Login errado', e);
+      throw new Error('Erro na requisicao');
+    }
+  };
+  useEffect(() => {
+    onInit();
+  }, []);
+  return (
+    <SafeAreaView style={styles.conteinerPai}>
+      <StatusBar backgroundColor="#FF9400" />
+      <View style={styles.conteinerImg}>
+        <Image source={require('../img/Logo.png')} style={styles.logo} />
+      </View>
+      <Text style={styles.textEmail}>Email</Text>
+      <View style={styles.inputAreaEmail}>
+        <TextInput
+          style={styles.inputEmail}
+          value={inputEmail}
+          onChangeText={(value) => setInputEmail(value)}
+        />
+      </View>
+      <Text style={styles.textSenha}>Senha</Text>
+      <View style={styles.inputArea}>
+        <TextInput
+          style={styles.input}
+          value={inputPassword}
+          onChangeText={(value) => setInputPassword(value)}
+          secureTextEntry={hidePass}
+        />
+        <TouchableOpacity
+          style={styles.icon}
+          onPress={() => setHidePass(!hidePass)}
+        >
+          <Ionicons name="eye" color="#BBB5AA" size={25} />
+        </TouchableOpacity>
+      </View>
 
-            </View>
+      <View style={styles.link}>
+        <TouchableOpacity onPress={() => navigation.replace('Cadastrar')}>
+          <Text style={styles.titleCadastrar}>Cadastrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.titleEsqueci}>Esqueci a senha</Text>
+        </TouchableOpacity>
+      </View>
 
-            <View style={styles.link}>
-                <TouchableOpacity
-                onPress={ () => navigation.replace('Cadastrar')}
-                >
-                    <Text style={styles.titleCadastrar}>Cadastrar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={styles.titleEsqueci}>Esqueci a senha</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.TextButton}>ENTRAR</Text>
-                </TouchableOpacity>
-            </View>
-
-        </SafeAreaView>
-)};
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            login;
+          }}
+          style={styles.button}
+        >
+          <Text style={styles.TextButton}>ENTRAR</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 export default Login;
 
 const styles = StyleSheet.create({
+  conteinerPai: {
+    flex: 1,
+    backgroundColor: '#464237',
+    fontFamily: 'Roboto',
+  },
+  inputAreaEmail: {
+    width: '70%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    marginHorizontal: 60,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+    marginBottom: 20,
+  },
+  inputEmail: {
+    width: '100%',
+    height: 41,
+    color: 'black',
+  },
+  inputArea: {
+    flexDirection: 'row',
+    width: '70%',
+    backgroundColor: '#F9F8F6',
+    height: 42,
+    alignItems: 'center',
+    marginHorizontal: 60,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#E8E6E2',
+    elevation: 20,
+  },
+  input: {
+    width: '85%',
+    height: 41,
+    color: 'black',
+    padding: 8,
+  },
+  icon: {
+    width: '15%',
+    height: 41,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textEmail: {
+    marginHorizontal: 60,
+    fontSize: 16,
+    color: '#BA872A',
+    fontWeight: '700',
+  },
 
-    conteinerPai:{
-        flex: 1,
-        backgroundColor:'#464237',
-        fontFamily:'Roboto',
+  textSenha: {
+    marginHorizontal: 60,
+    fontSize: 16,
+    color: '#BA872A',
+    fontWeight: '700',
+  },
 
-    },
-    inputAreaEmail:{
-        width:'70%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        marginHorizontal:60,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-        marginBottom:20,
-    },
-    inputEmail:{
-        width:'93%',
-        height: 41,
-        color:'black',
-    },
-    inputArea:{
-        flexDirection:'row',
-        width:'70%',
-        backgroundColor:'#F9F8F6',
-        height: 42,
-        alignItems:'center',
-        marginHorizontal:60,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#E8E6E2',
-        elevation: 20,
-        
-    }, 
-    input:{
-        width:'85%',
-        height: 41,
-        color:'black',
-        padding: 8,
-    },
-    icon:{
-        width:'15%',
-        height:41,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    textEmail:{
-        marginHorizontal:60,
-        fontSize: 16,
-        color:'#BA872A',
-        fontWeight: '700',
-    },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  },
 
-    textSenha:{
-        marginHorizontal:60,
-        fontSize: 16,
-        color:'#BA872A',
-        fontWeight: '700',
+  TextButton: {
+    backgroundColor: '#FF9400',
+    paddingVertical: 20,
+    paddingHorizontal: 76,
+    color: '#000000',
+    fontSize: 16,
+    fontWeight: 'bold',
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#B18739',
+    elevation: 20,
+  },
 
-    },
+  conteinerImg: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 110,
+    elevation: 20,
+  },
 
-    button:{
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop: 30,
-    },
+  logo: {
+    width: 192,
+    height: 192,
+  },
 
-    TextButton:{
-        backgroundColor:'#FF9400',
-        paddingVertical:20,
-        paddingHorizontal:76,
-        color:'#000000',
-        fontSize: 16,
-        fontWeight: 'bold',
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor:'#B18739',
-        elevation: 20,
-    },
+  titleCadastrar: {
+    color: '#BA872A',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  titleEsqueci: {
+    color: '#BA872A',
+    fontSize: 16,
+    marginLeft: 100,
+    fontWeight: '600',
+  },
 
-    conteinerImg:{
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop:110,
-        elevation: 20,
-    },
-
-    logo:{
-        width:192,
-        height:192,
-    },
-
-    titleCadastrar:{
-        color:'#BA872A',
-        fontSize: 16,
-        fontWeight: '600',
-    },
-    titleEsqueci:{
-        color:'#BA872A',
-        fontSize: 16,
-        marginLeft:100,
-        fontWeight: '600',
-        
-    },
-
-    link:{
-        flexDirection:'row',
-        marginHorizontal: 60,
-        marginBottom:3,
-        marginTop:10,
-    },
-
+  link: {
+    flexDirection: 'row',
+    marginHorizontal: 60,
+    marginBottom: 3,
+    marginTop: 10,
+  },
 });
